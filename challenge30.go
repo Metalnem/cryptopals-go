2f8fbd99359f904514288ae2a2fbe494b9841812
Metalnem <metalnem@mijailovic.net>
2016-06-11 15:52:21 +0200

    Add solution and test for challenge 30 (Break an MD4 keyed MAC using length extension)

--- challenge30.go
+++ challenge30.go
@@ -4,24 +4,75 @@
 package main
 
 import (
-	"crypto/sha1"
-	"hash"
+	"crypto/subtle"
+	"encoding/binary"
 )
 
-func macSha1(key, message []byte) []byte {
-	h := sha1.New()
+type challenge30 struct {
+}
 
-	h.Write(key)
+type macMD4Server struct {
+	key []byte
+}
+
+func (s *macMD4Server) sign(message []byte) []byte {
+	h := New()
+
+	h.Write(s.key)
 	h.Write(message)
 
 	return h.Sum(nil)
 }
 
-func forge(s [4]uint32) hash.Hash {
+func (s *macMD4Server) validate(message, mac []byte) bool {
+	return subtle.ConstantTimeCompare(s.sign(message), mac) == 1
+}
+
+func (challenge30) forgePadding(keyLength int, message []byte) []byte {
+	size := keyLength + len(message)
+	padLen := 64 - size%64
+
+	if size%64 >= 56 {
+		padLen += 64
+	}
+
+	padding := make([]byte, padLen)
+	padding[0] = 0x80
+	binary.LittleEndian.PutUint64(padding[padLen-8:], uint64(8*size))
+
+	return padding
+}
+
+func (challenge30) forgeHash(mac []byte, len uint64) *digest {
 	d := new(digest)
-
 	d.Reset()
-	copy(d.s[:], s[:])
 
+	for i := 0; i < 4; i++ {
+		from, to := 4*i, 4*(i+1)
+		d.s[i] = binary.LittleEndian.Uint32(mac[from:to])
+	}
+
+	d.len = len
 	return d
+}
+
+func (x challenge30) ForgeMessage(s *macMD4Server, message, suffix []byte) []byte {
+	mac := s.sign(message)
+
+	for keyLength := 1; ; keyLength++ {
+		padding := x.forgePadding(keyLength, message)
+		len := keyLength + len(message) + len(padding)
+
+		h := x.forgeHash(mac, uint64(len))
+		h.Write(suffix)
+
+		var forged []byte
+		forged = append(forged, message...)
+		forged = append(forged, padding...)
+		forged = append(forged, suffix...)
+
+		if s.validate(forged, h.Sum(nil)) {
+			return forged
+		}
+	}
 }
---
+++ challenge30_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/binary"
+	"reflect"
+	"testing"
+)
+
+func TestForgeMessage(t *testing.T) {
+	server := &macMD4Server{
+		key: []byte("We all live in a yellow submarine"),
+	}
+
+	message := []byte("comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon")
+	suffix := []byte(";admin=true")
+
+	size := len(server.key) + len(message)
+	padding := make([]byte, 128-size)
+
+	padding[0] = 0x80
+	binary.LittleEndian.PutUint64(padding[len(padding)-8:], uint64(8*size))
+
+	var expected []byte
+	actual := challenge30{}.ForgeMessage(server, message, suffix)
+
+	expected = append(expected, message...)
+	expected = append(expected, padding...)
+	expected = append(expected, suffix...)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("Expected %v, was %v", expected, actual)
+	}
+}
