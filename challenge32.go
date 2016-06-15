3293d7b346b40f8945e874807950a644ad21a4f6
Metalnem <metalnem@mijailovic.net>
2016-06-15 22:09:21 +0200

    Add solution for challenge 32

---
+++ challenge32.go
@@ -0,0 +1,45 @@
+// Challenge 31 - Implement and break HMAC-SHA1 with an artificial timing leak
+// http://cryptopals.com/sets/4/challenges/31
+
+package cryptopals
+
+import (
+	"crypto/sha1"
+	"net/http"
+	"time"
+)
+
+type challenge32 struct {
+}
+
+func (challenge32) ForgeHmacSHA1SignaturePrecise(addr, file string) []byte {
+	sig := make([]byte, sha1.Size)
+	x := challenge31{}
+
+	for i := 0; i < len(sig); i++ {
+		var valBest byte
+		var timeBest time.Duration
+
+		for j := 0; j < 256; j++ {
+			sig[i] = byte(j)
+			url := x.buildURL(addr, file, sig)
+			start := time.Now()
+
+			for k := 0; k < 15; k++ {
+				resp, _ := http.Get(url)
+				resp.Body.Close()
+			}
+
+			elapsed := time.Since(start)
+
+			if elapsed > timeBest {
+				valBest = byte(j)
+				timeBest = elapsed
+			}
+		}
+
+		sig[i] = valBest
+	}
+
+	return sig
+}
--- challenge32_test.go
+++ challenge32_test.go
@@ -8,11 +8,11 @@ import (
 	"time"
 )
 
-func TestForgeHmacSHA1Signature(t *testing.T) {
+func TestForgeHmacSHA1SignaturePrecise(t *testing.T) {
 	t.Skip("Skip this test, because it runs too long")
 
 	key := []byte("We all live in a yellow submarine")
-	delay := 50 * time.Millisecond
+	delay := 5 * time.Millisecond
 	s := &hmacSHA1Server{key: key, delay: delay}
 
 	go func() {
@@ -25,7 +25,7 @@ func TestForgeHmacSHA1Signature(t *testing.T) {
 	h := hmac.New(sha1.New, key)
 	h.Write([]byte(file))
 
-	signature := challenge31{}.ForgeHmacSHA1Signature("http://localhost:9000/test", file)
+	signature := challenge32{}.ForgeHmacSHA1SignaturePrecise("http://localhost:9000/test", file)
 	expected := hex.EncodeToString(h.Sum(nil))
 	actual := hex.EncodeToString(signature)
 
