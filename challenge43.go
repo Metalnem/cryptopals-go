f6ccea8d7b31d8b23fa0c3c2cc9613d8765b244f
Metalnem <metalnem@mijailovic.net>
2016-07-20 22:16:57 +0200

    Implement challenge 43 (DSA key recovery from nonce)

---
+++ challenge43.go
@@ -0,0 +1,34 @@
+// Challenge 43 - DSA key recovery from nonce
+// http://cryptopals.com/sets/6/challenges/43
+
+package cryptopals
+
+import (
+	"errors"
+	"math/big"
+)
+
+type challenge43 struct {
+}
+
+func (challenge43) RecoverDsaKeyFromNonce(data []byte, pub *dsaPublicKey, signature *dsaSignature) (*dsaPrivateKey, error) {
+	priv := &dsaPrivateKey{dsaPublicKey: *pub}
+	var i int64
+
+	for i = 0; i < 65536; i++ {
+		k := big.NewInt(i)
+
+		x := new(big.Int).Mul(signature.s, k)
+		x = x.Sub(x, dsaHash(data))
+		x = x.Mul(x, new(big.Int).ModInverse(signature.r, pub.q))
+		x = x.Mod(x, pub.q)
+
+		priv.x = x
+
+		if priv.verify(data, signature) {
+			return priv, nil
+		}
+	}
+
+	return nil, errors.New("Failed to recover DSA private key from nonce")
+}
