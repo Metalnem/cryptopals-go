c33e5d19b4a0d6fbcee18f6cbe6aafb11a027876
Metalnem <metalnem@mijailovic.net>
2016-07-20 21:59:04 +0200

    Add default DSA parameters

--- dsa.go
+++ dsa.go
@@ -26,6 +26,30 @@ type dsaSignature struct {
 	s *big.Int
 }
 
+func defaultDsaParameters() dsaParameters {
+	var p = new(big.Int)
+	var q = new(big.Int)
+	var g = new(big.Int)
+
+	p.SetString("800000000000000089e1855218a0e7dac38136ffafa72eda7"+
+		"859f2171e25e65eac698c1702578b07dc2a1076da241c76c6"+
+		"2d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebe"+
+		"ac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2"+
+		"b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc87"+
+		"1a584471bb1", 16)
+
+	q.SetString("f4f47f05794b256174bba6e9b396a7707e563c5b", 16)
+
+	g.SetString("5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119"+
+		"458fef538b8fa4046c8db53039db620c094c9fa077ef389b5"+
+		"322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a047"+
+		"0f5b64c36b625a097f1651fe775323556fe00b3608c887892"+
+		"878480e99041be601a62166ca6894bdd41a7054ec89f756ba"+
+		"9fc95302291", 16)
+
+	return dsaParameters{p: p, q: q, g: g}
+}
+
 func generateDsaPrivateKey(params dsaParameters) *dsaPrivateKey {
 	x := new(big.Int)
 
